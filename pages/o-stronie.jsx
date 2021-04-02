db3685a6406e4e627734b1c8cb5f72366eb969de
OreoPL-studio <81591050+OreoPL-studio@users.noreply.github.com>
2021-04-02 21:19:06 +0200

    Zaktualizowano opis strony
    rzaby so piekne

--- pages/o-stronie.jsx
+++ pages/o-stronie.jsx
@@ -4,7 +4,9 @@ export default function About()
 {
     return(
         <div className={styles.main}>
-            nvifdsukbdsv
+            <h1>Strona oraz baza danych zrobiona przez Piotra Nowakowskiego i Pawła Floryana. 
+    Postanowiliśmy utworzyć tę stronę w celu uzyskania wyżeszej oceny całorocznej w pierwszej klasie techinkum na przedmiocie pracownia baz danych.
+    Była to również świetna okazja do nauki nowych czynności związanych z zarządzaniem bazami danych oraz z programowaniem.</h1>
         </div>
     )
 }
