18f70a982a13b52bdea90630d68b194f9c27fad3
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-04-06 01:35:29 +0200

    Dodano formularz do pisania kodu SQL

---
+++ components/SqlForm.jsx
@@ -0,0 +1,15 @@
+import styles from "../styles/SqlForm.module.css";
+
+export default function SqlForm()
+{
+    const securityPattern = /(drop)|(delete)|(create)|(use)/gi
+    return(
+        <fieldset className={styles.main}>
+            <legend>SQL</legend>
+            <form action="" method="post">
+                <textarea name="q" cols="30" rows="10" onChange={(e) => {if (securityPattern.test(e.target.value)) alert("Nawet nie próbuj")}}></textarea>
+                <input type="submit" value="Wykonaj"/>
+            </form>
+        </fieldset>
+    )
+}
--- pages/sql.jsx
+++ pages/sql.jsx
@@ -1,10 +1,11 @@
+import SqlForm from '../components/SqlForm'
 import styles from '../styles/Sql.module.css'
 
 export default function Sql(props)
 {
     return(
         <div>
-
+            <SqlForm/>
         </div>
     )
 }
