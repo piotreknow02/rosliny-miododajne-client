4a96207713b251eaf59772ae7dd15469346903b1
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-04-06 00:37:42 +0200

    Dodano autorów strony

---
+++ components/DeveloperIcons.jsx
@@ -0,0 +1,27 @@
+import styles from "../styles/DeveloperIcons.module.css";
+
+export default function DeveloperIcons() {
+    const devicons = [
+        {
+        username: "piotreknow02",
+        photo: "https://avatars.githubusercontent.com/u/65082017",
+    },
+    {
+        username: "OreoPL-Studio",
+        photo: "https://avatars.githubusercontent.com/u/81591050",
+    }
+    ]
+    return (
+        <fieldset className={styles.main}>
+            <legend>Autorzy strony</legend>
+            {devicons.map(i =>
+                <a href={"https://github.com/" + i.username}>
+                    <div className={styles.devicon} title={i.username}>
+                        <img src={i.photo} alt={i.username}/>
+                    </div>
+                </a>
+            )}
+
+        </fieldset>
+    )
+}
