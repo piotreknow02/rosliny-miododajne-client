1db9303af4a16a6f1ca017825f3c0a8e8fbfe75b
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-04-06 00:38:11 +0200

    Dodano ikony social media

--- pages/o-stronie.jsx
+++ pages/o-stronie.jsx
@@ -1,19 +1,16 @@
+import DeveloperIcons from '../components/DeveloperIcons'
+import SocialmediaIcons from '../components/SocialmediaIcons'
 import styles from '../styles/O-stronie.module.css'
 
 export default function About() {
     return (
         <div className={styles.main}>
             <h1>Strona oraz baza danych zrobiona przez Piotra Nowakowskiego i Pawła Floryana.
-    <p className={styles.akapit}>Postanowiliśmy utworzyć tę stronę w celu uzyskania wyżeszej oceny całorocznej w pierwszej klasie techinkum na przedmiocie pracownia baz danych.</p>
-    Była to również świetna okazja do nauki nowych czynności związanych z zarządzaniem bazami danych oraz z programowaniem. <p>Na stronie znajdują się sekcje "Baza" oraz "Edytor SQL".</p> W pierwszej zgodnie z nazwą znajduje się baza danych, a w drugiej funkcjonalny dla użytkownika edytor kodu sql. <p>Miłego kożystania!👋</p></h1>
+            <p className={styles.akapit}>Postanowiliśmy utworzyć tę stronę w celu uzyskania wyżeszej oceny całorocznej w pierwszej klasie techinkum na przedmiocie pracownia baz danych.</p>
+            Była to również świetna okazja do nauki nowych czynności związanych z zarządzaniem bazami danych oraz z programowaniem. <p>Na stronie znajdują się sekcje "Baza" oraz "Edytor SQL".</p> W pierwszej zgodnie z nazwą znajduje się baza danych, a w drugiej funkcjonalny dla użytkownika edytor kodu sql. <p>Miłego kożystania!👋</p></h1>
 
-
-            <a href="https://www.facebook.com/Rośliny-miododajne-111259011066259">
-                <i className={styles.ikona + " fab fa-facebook-square"} ></i>
-            </a>
-            <a href="mailto: roslinymiododajne@poczta.fm">
-                <i className={styles.ikona + " fas fa-envelope"} ></i>
-            </a>
+            <DeveloperIcons/>
+            <SocialmediaIcons/>
         </div>
     )
 }
