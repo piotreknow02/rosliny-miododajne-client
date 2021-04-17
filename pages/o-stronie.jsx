6cf0ce96dd98b8817a62703a6dfa1f6ec37c9274
OreoPL-studio <81591050+OreoPL-studio@users.noreply.github.com>
2021-04-17 22:58:44 +0200

    Update o-stronie.jsx

--- pages/o-stronie.jsx
+++ pages/o-stronie.jsx
@@ -6,8 +6,8 @@ export default function About() {
     return (
         <div className={styles.main}>
             <h1>Strona oraz baza danych zrobiona przez Piotra Nowakowskiego i Pawła Floryana.
-            <p className={styles.akapit}>Postanowiliśmy utworzyć tę stronę w celu uzyskania wyżeszej oceny całorocznej w pierwszej klasie techinkum na przedmiocie pracownia baz danych.</p>
-            Była to również świetna okazja do nauki nowych czynności związanych z zarządzaniem bazami danych oraz z programowaniem. <p>Na stronie znajdują się sekcje "Baza" oraz "Edytor SQL".</p> W pierwszej zgodnie z nazwą znajduje się baza danych, a w drugiej funkcjonalny dla użytkownika edytor kodu sql. <p>Miłego kożystania!👋</p></h1>
+            <p className={styles.akapit}>Postanowiliśmy utworzyć tę stronę w celu uzyskania wyższej oceny całorocznej w pierwszej klasie technikum na przedmiocie pracownia baz danych.</p>
+            Była to również świetna okazja do nauki nowych czynności związanych z zarządzaniem bazami danych oraz z programowaniem. <p>Na stronie znajdują się sekcje "Baza" oraz "Edytor SQL".</p> W pierwszej zgodnie z nazwą znajduje się baza danych, a w drugiej funkcjonalny dla użytkownika edytor kodu sql. <p>Miłego korzystania!👋</p></h1>
 
             <DeveloperIcons/>
             <SocialmediaIcons/>
