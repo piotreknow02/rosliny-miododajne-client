7dd56a9f09c95e730f5418e1de456ef04b1bcc81
n00sek <n00sek@onet.pl>
2022-06-05 20:27:08 +0200

    a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,6 @@ export default function Navbar(props)
                     <NavLink href="/o-stronie"><h3>O stronie</h3></NavLink>
                     <NavLink href="/baza"><h3>Baza</h3></NavLink>
                     <NavLink href="/galeria"><h3>Galeria</h3></NavLink>
-                    <NavLink href="/sql"><h3>Edytor SQL</h3></NavLink>
                     <li>
                         <div className={styles.themebtn} onClick={() => {setIsDark(!isDark); localStorage.setItem("isDark", !isDark)}}>
                             <i className={isDark ? "fas fa-sun": "fas fa-moon"}></i>
--- src/pages/o-stronie.jsx
+++ src/pages/o-stronie.jsx
@@ -8,9 +8,9 @@ export default function About() {
     return (
         <Layout>
             <div className={styles.main}>
-                <h1>Strona oraz baza danych zrobiona przez Piotra Nowakowskiego i Pawła Floryana.
-                <p className={styles.akapit}>Postanowiliśmy utworzyć tę stronę w celu uzyskania wyżeszej oceny całorocznej w pierwszej klasie techinkum na przedmiocie pracownia baz danych.</p>
-                Była to również świetna okazja do nauki nowych czynności związanych z zarządzaniem bazami danych oraz z programowaniem. <p>Na stronie znajdują się sekcje "Baza" oraz "Edytor SQL".</p> W pierwszej zgodnie z nazwą znajduje się baza danych, a w drugiej funkcjonalny dla użytkownika edytor kodu sql. <p>Miłego kożystania!👋</p></h1>
+                <h1>Strona oraz baza danych zrobiona przez Piotra Nowakowskiego, Pawła Floryana i Bartosza Mółkę.
+                <p className={styles.akapit}>Postanowiliśmy utworzyć tę stronę w celu uzyskania wyżeszej oceny całorocznej w drugiej klasie techinkum na przedmiocie systemy baz danych.</p>
+                Była to również świetna okazja do nauki nowych czynności związanych z zarządzaniem bazami danych oraz z programowaniem. <p>Na stronie znajdują się sekcje "Baza" oraz "Edytor SQL".</p> W pierwszej zgodnie z nazwą znajduje się baza danych, a w drugiej funkcjonalny dla użytkownika edytor kodu sql. <p>Miłego korzystania!👋</p></h1>
                 <DeveloperIcons/>
             </div>
         </Layout>
